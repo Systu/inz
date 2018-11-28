f2bb3a61dae8580775462960c88844ab016646b1
systu <hubert.syst@gmail.com>
2018-11-28 19:27:10 +0100

    ending DietListmodel

---
+++ inzynierka/Models/DietListViewModels/DeleteViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace inzynierka.Models.DietListViewModels
+{
+    public class DeleteViewModel
+    {
+        public Guid DietListId { get; set; }
+        [Display(Name = "Nazwa diety")]
+        public string DietName { get; set; }
+        [Display(Name = "Opis")]
+        public string Describe { get; set; }
+        [Display(Name = "Data dodania")]
+        public DateTime AddedTime { get; set; }
+    }
+}
---
+++ inzynierka/Models/DietListViewModels/EditViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace inzynierka.Models.DietListViewModels
+{
+    public class EditViewModel
+    {
+        public Guid DietListId { get; set; }
+        [Display(Name = "Nazwa diety")]
+        public string DietName { get; set; }
+        [Display(Name = "Opis")]
+        public string Describe { get; set; }
+    }
+}
